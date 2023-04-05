c43a35be72198290b910012751644495a51cd744
Epharus Ndumia <epharusndumia@gmail.com>
2023-04-05 14:34:07 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+input.addEventListener('click',addingEventListener)
+
+function addingEventListener(event){
+  if(event === input){
+    alert('I was clicked!');
+
+  }else{
+    alert('add event listener');
+  }
 }
